5c8fb827cebcbe8906e78164796f47ba1e4cb2b6
Lexi <lexa.shelby@gmail.com>
2019-08-29 12:51:04 -0400

    Lab 23 SWAPI first commit

---
+++ Lab23SWAPI/Controllers/SWController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Lab23SWAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab23SWAPI.Controllers
+{
+    public class SWController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<Person> GetPersonById(int index)
+        {
+            var client = GetHttpClient();
+            var response = await client.GetAsync($"api/people/{index}");
+            var person = await response.Content.ReadAsAsync<Person>();           
+            return person;
+        }
+        public async Task<Planet> GetPlanetById(int index)
+        {
+            var client = GetHttpClient();
+
+            var response = await client.GetAsync($"api/planets/{index}");
+            var planet = await response.Content.ReadAsAsync<Planet>();
+            return planet;
+        }
+        public static HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.co");
+            return client;
+        }
+        public IActionResult ResultPlanet(int index)
+        {
+            var planet = GetPlanetById(index).Result;
+            return View(planet);
+        }
+        public IActionResult ResultPerson(int index)
+        {
+            var person = GetPersonById(index).Result;
+            return View(person);
+        }
+    }
+}
---
+++ Lab23SWAPI/Models/SWAPI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab23SWAPI.Models
+{
+    public class PersonList
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public object previous { get; set; }
+        public Person[] results { get; set; }
+    }
+
+    public class Person
+    {
+        public string name { get; set; }
+        public string height { get; set; }
+        public string mass { get; set; }
+        public string hair_color { get; set; }
+        public string skin_color { get; set; }
+        public string eye_color { get; set; }
+        public string birth_year { get; set; }
+        public string gender { get; set; }
+        public string homeworld { get; set; }
+        public string[] films { get; set; }
+        public string[] species { get; set; }
+        public string[] vehicles { get; set; }
+        public string[] starships { get; set; }
+        public DateTime created { get; set; }
+        public DateTime edited { get; set; }
+        public string url { get; set; }
+    }
+
+    public class PlanetList
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public object previous { get; set; }
+        public Planet[] planet { get; set; }
+    }
+
+    public class Planet
+    {
+        public string name { get; set; }
+        public string rotation_period { get; set; }
+        public string orbital_period { get; set; }
+        public string diameter { get; set; }
+        public string climate { get; set; }
+        public string gravity { get; set; }
+        public string terrain { get; set; }
+        public string surface_water { get; set; }
+        public string population { get; set; }
+        public string[] residents { get; set; }
+        public string[] films { get; set; }
+        public DateTime created { get; set; }
+        public DateTime edited { get; set; }
+        public string url { get; set; }
+    }
+}
